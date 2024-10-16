0ce6d360fd34e17c4382acedef272ef3c53a8017
ryongseong <xmssnsk2119@gmail.com>
2024-10-16 09:58:40 +0900

    241015

---
+++ ch2/craco.config.js
@@ -0,0 +1,13 @@
+const CreateAlias = require('craco-alias')
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CreateAlias,
+            options: {
+                source: 'tsconfig',
+                tsConfigPath: 'tsconfig.paths.json',
+            },
+        },
+    ],
+}
