2da68f7fed15afb28de4fef97be2bf611936897e
ryongseong <ryongseong.dev@gmail.com>
2025-03-27 23:08:31 +0900

    Add - Part2_ch4_06

--- part1_wedding/src/App.tsx
+++ part1_wedding/src/App.tsx
@@ -2,32 +2,39 @@ import classNames from 'classnames/bind'
 import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
+import FullScreenMessage from '@shared/FullScreenMessage'
 
-import FullScreenMessage from './components/shared/FullScreenMessage'
+import Heading from './components/sections/Heading'
+import Video from './components/sections/Video'
+
+import { Wedding } from '@models/wedding'
+import ImageGallery from './components/sections/ImageGallery'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  const [wedding, setWedding] = useState(null)
+  const [wedding, setWedding] = useState<Wedding | null>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
 
-  // 1. wedding data 호출
+  // 1. wedding 데이터 호출
   useEffect(() => {
     setLoading(true)
 
     fetch('http://localhost:8888/wedding')
-      .then((res) => {
-        if (res.ok === false) {
+      .then((response) => {
+        if (!response.ok) {
           throw new Error('청첩장 정보를 불러오지 못했습니다.')
         }
-        return res.json()
+
+        return response.json()
       })
       .then((data) => {
         setWedding(data)
       })
-      .catch((e) => {
-        console.log(e)
+      .catch((error) => {
+        console.log('에러 발생', error)
+
         setError(true)
       })
       .finally(() => {
@@ -43,7 +50,20 @@ function App() {
     return <FullScreenMessage type="error" />
   }
 
-  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
+  if (wedding === null) {
+    return null
+  }
+
+  const { date, galleryImages } = wedding
+
+  return (
+    <div className={cx('container')}>
+      <Heading date={date} />
+      <Video />
+      <ImageGallery images={galleryImages} />
+      {JSON.stringify(wedding)}
+    </div>
+  )
 }
 
 export default App
