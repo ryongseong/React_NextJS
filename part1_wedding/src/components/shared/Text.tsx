a8f4f9391ab82e3f1428e06b2ba1977b99b9c64c
ryongseong <ryongseong.dev@gmail.com>
2025-03-28 17:39:10 +0900

    Add - Part2_ch4_08

---
+++ part1_wedding/src/components/shared/Text.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Text({ children }: { children: string }) {
+  const message = children.split('\n').map((str, idx, array) => {
+    return (
+      <React.Fragment key={idx}>
+        {str}
+        {idx === array.length - 1 ? null : <br />}
+      </React.Fragment>
+    )
+  })
+
+  console.log(message)
+
+  return <div>{message}</div>
+}
+
+export default Text
