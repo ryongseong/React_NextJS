072bf946469fc19cece9839abb3cfbe18095f0ea
ryongseong <ryongseong.dev@gmail.com>
2025-05-12 14:32:11 +0900

    Add - Part2_ch2_03

--- part2_mcard/src/App.tsx
+++ part2_mcard/src/App.tsx
@@ -1,14 +1,27 @@
 import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 import logo from './logo.svg'
 import './App.css'
 
+const bold = css`
+  font-weight: bold;
+`
+
 const containerStyles = css`
   background-color: pink;
+  ${bold};
+`
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
+  ${bold};
 `
 
 function App() {
   return (
     <div className="App" css={containerStyles}>
+      <Button>Style Button</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ part2_mcard/src/styles/typography.ts
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react'
+
+export const typographyMap = {
+  t1: css`
+    font-size: 30px;
+    line-height: 1.35;
+  `,
+  t2: css`
+    font-size: 26px;
+    line-height: 1.4;
+  `,
+  t3: css`
+    font-size: 30px;
+    line-height: 1.35;
+  `,
+  t4: css`
+    font-size: 30px;
+    line-height: 1.35;
+  `,
+  t5: css`
+    font-size: 30px;
+    line-height: 1.35;
+  `,
+  t6: css`
+    font-size: 30px;
+    line-height: 1.35;
+  `,
+  t7: css`
+    font-size: 30px;
+    line-height: 1.35;
+  `,
+}
